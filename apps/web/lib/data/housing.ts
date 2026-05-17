86209fe32aa4e609116c5c41d9cd4c94eab40f0e
achalnm <achalnm02@gmail.com>
2026-05-17 17:30:00 +0100

    fix chart data

--- apps/web/lib/data/housing.ts
+++ apps/web/lib/data/housing.ts
@@ -17,7 +17,7 @@ export function computeHousingStats(data: HousingDataPoint[]): HousingStats {
     return { nationalMedianPrice: 0, dublinMedianPrice: 0, yoyChangeNational: 0, yoyChangeDublin: 0 };
   }
   const latest = data[data.length - 1];
-  const previousYear = data[data.length - 5];
+  const previousYear = data[data.length - 5] ?? data[0];
   const yoyNational =
     ((latest.nationalMedianPrice - previousYear.nationalMedianPrice) /
       previousYear.nationalMedianPrice) *
